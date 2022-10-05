a7a5fdca6be295d3a47ca6b1b2c0abe27a7ce73b
Christian Segercrantz <segercc1@aalto.fi>
2022-10-05 18:35:54 +0300

    Updated plot font sizes

--- Assignment  1/MATLAB-files/ex3.m
+++ Assignment  1/MATLAB-files/ex3.m
@@ -1,26 +1,29 @@
 %% Setup
 clear all, close all
+%% Get working directory 
+currentFolder = pwd;
 %% constants
-    m = 100;
-    S = 14;
-    C_d0 = 0.034;
-    K = 0.07;
-    g = 9.81;
-    rho = 1.13;
-    C_l = 0.7; %0.696932;
-    c = [m S C_d0 K g rho C_l];
+n0 = 4;
+m = 100;
+S = 14;
+C_d0 = 0.034;
+K = 0.07;
+g = 9.81;
+rho = 1.13;
+C_l = 1; %0.696932;
+c = [m S C_d0 K g rho C_l];
 
 %% Initial values
 
-tspan = linspace(0,100,2000);
+tspan = linspace(0,1000,10000);
 
 stalling_speed = sqrt((g*2*m)/(C_l*S*rho))
 
-n0 = 4;
+
 x0 = linspace(0,0,n0);
-h0 = linspace(10,10,n0);
-v0 = linspace(1,stalling_speed*1.5,n0);
-gamma0 = linspace(0,pi*1/4,n0);
+h0 = linspace(50,50,n0);
+v0 = [0.3*stalling_speed 0.6*stalling_speed stalling_speed stalling_speed*1.3 ];%linspace(1,stalling_speed*1.2,n0);
+gamma0 = linspace(-pi*0/8,pi*0/8,n0);
 t = zeros(length(tspan),n0);
 y = zeros(length(tspan),n0,4);
 
@@ -35,39 +38,52 @@ for i = 1:n0
     t(length(t_temp):end,i) = t_temp(end);
     y(1:length(y_temp),i,:) = y_temp;
     y(length(y_temp):end,i,:) = repmat(y_temp(end,:),length(tspan)- length(y_temp)+1,1);
-    lables(i) = sprintf("$v_0 = %.1f m/s, \\gamma_0 = %.1f$", round(v0(i),1), round(rad2deg(gamma0(i)),1));
+    lables(i) = sprintf("$v_0 = %.1f m/s$", v0(i));%sprintf("$v_0 = %.1f m/s, \\gamma_0 = %.1f$", round(v0(i),1), round(rad2deg(gamma0(i)),1));
 end
 
 %% Plotting
 figure;
 hold on
-plot(y(:,:,1),y(:,:,2))
-xlabel('x');
-ylabel('h');
+plot(y(:,:,1),y(:,:,2),'LineWidth',1.5)
+ax = gca;
+ax.FontSize = 11;
+xlabel('$x$','Interpreter','latex','FontSize',13);
+ylabel('$h$','Interpreter','latex','FontSize',13);
 ylim([0, inf])
-legend(lables,'Interpreter','latex')
+legend(lables,'Interpreter','latex','FontSize',13)
+
 hold off
+saveas(gcf, "Plots\stalling_speed_h_1.png")
 
 figure;
 hold on
-plot(t, y(:,:,3))
-xlabel('$t$', 'Interpreter','latex');
-ylabel('$\dot{v}$', 'Interpreter','latex');
-legend(lables,'Interpreter','latex')
+plot(t, y(:,:,3),'LineWidth',1.5)
+ax = gca;
+ax.FontSize = 11;
+xlabel('$t$', 'Interpreter','latex','FontSize',13);
+ylabel('$\dot{v}$', 'Interpreter','latex','FontSize',13);
+legend(lables,'Interpreter','latex','FontSize',13)
+saveas(gcf, "Plots\stalling_speed_vdot_1.png")
 
 figure;
 hold on
-plot(t,y(:,:,3).*sin(y(:,:,4)))
-xlabel('$t$', 'Interpreter','latex');
-ylabel('$\dot{h}$', 'Interpreter','latex');
-legend(lables,'Interpreter','latex')
+plot(t,y(:,:,3).*sin(y(:,:,4)),'LineWidth',1.5)
+ax = gca;
+ax.FontSize = 11;
+xlabel('$t$', 'Interpreter','latex','FontSize',13);
+ylabel('$\dot{h}$', 'Interpreter','latex','FontSize',13);
+legend(lables,'Interpreter','latex','FontSize',13)
+saveas(gcf, "Plots\stalling_speed_hdot_1.png")
 
 figure;
 hold on
-plot(t,y(:,:,3).*cos(y(:,:,4)))
-xlabel('$t$', 'Interpreter','latex');
-ylabel('$\dot{x}$', 'Interpreter','latex');
-legend(lables,'Interpreter','latex')
+plot(t,y(:,:,3).*cos(y(:,:,4)),'LineWidth',1.5)
+ax = gca;
+ax.FontSize = 11;
+xlabel('$t$', 'Interpreter','latex','FontSize',13);
+ylabel('$\dot{x}$', 'Interpreter','latex','FontSize',13);
+legend(lables,'Interpreter','latex','FontSize',13)
+saveas(gcf, "Plots\stalling_speed_xdot_1.png")
 
 %% ODE function
 function ret = odefcn(y, c)
