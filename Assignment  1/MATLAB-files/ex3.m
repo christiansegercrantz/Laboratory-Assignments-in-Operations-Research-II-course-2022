bc969136800d16a2113265a3ce1b1114ef12159e
Christian Segercrantz <segercc1@aalto.fi>
2022-10-05 11:47:54 +0300

    Fixed solver to stop at h=0

--- Assignment  1/MATLAB-files/ex3.m
+++ Assignment  1/MATLAB-files/ex3.m
@@ -7,34 +7,37 @@ clear all, close all
     K = 0.07;
     g = 9.81;
     rho = 1.13;
-    C_l = 0.696932;
+    C_l = 0.7; %0.696932;
     c = [m S C_d0 K g rho C_l];
 
 %% Initial values
 
-tspan = linspace(0,150,10000);
+tspan = linspace(0,100,2000);
 
 stalling_speed = sqrt((g*2*m)/(C_l*S*rho))
 
 n0 = 4;
 x0 = linspace(0,0,n0);
 h0 = linspace(10,10,n0);
-v0 = linspace(1,stalling_speed*2,n0);
-gamma0 = linspace(0,pi*3/8,n0);
+v0 = linspace(1,stalling_speed*1.5,n0);
+gamma0 = linspace(0,pi*1/4,n0);
 t = zeros(length(tspan),n0);
 y = zeros(length(tspan),n0,4);
 
-%opts = odeset('NonNegative',[1 2]);
+opts = odeset('Events',@heightZeroEventsFcn);
 
 %% ODE solver
 for i = 1:n0
     y0 = [x0(i) h0(i) v0(i) gamma0(i)];
-    [t_temp,y_temp] = ode45(@(t,y) odefcn(y,c), tspan, y0);
-    t_temp;
-    t(:,i) = t_temp;
-    y(:,i,:) = y_temp;
+    [t_temp, y_temp, te_temp, ye_temp, ie_temp] = ode45(@(t,y) odefcn(y,c), tspan, y0, opts);
+    %size(t_temp)
+    t(1:length(t_temp),i) = t_temp;
+    t(length(t_temp):end,i) = t_temp(end);
+    y(1:length(y_temp),i,:) = y_temp;
+    y(length(y_temp):end,i,:) = repmat(y_temp(end,:),length(tspan)- length(y_temp)+1,1);
     lables(i) = sprintf("$v_0 = %.1f m/s, \\gamma_0 = %.1f$", round(v0(i),1), round(rad2deg(gamma0(i)),1));
 end
+
 %% Plotting
 figure;
 hold on
@@ -80,4 +83,10 @@ function ret = odefcn(y, c)
     v_dot = -(S*rho)/(2*m)*(C_d0 + K*C_l^2)*y(3)^2-g*sin(y(4));
     gamma_dot = 1/(2*m)*C_l*S*rho*y(3)-g/y(3)*cos(y(4));
     ret = [x_dot; h_dot; v_dot; gamma_dot];
+end
+%Stopping function
+function [position,isterminal,direction] = heightZeroEventsFcn(t,y)
+  position = y(2); % The value that we want to be zero
+  isterminal = 1;  % Halt integration 
+  direction = 0;   % The zero can be approached from either direction
 end
